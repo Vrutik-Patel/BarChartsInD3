0a3603affbc7b8833a24160d8fa9d7e75a64ca12
Chris Wyble <cwgiant@gmail.com>
2021-02-10 23:42:44 -0600

    Updated python script for cleaner data and basic HTML edits

--- frequencyCounter.py
+++ frequencyCounter.py
@@ -3,33 +3,36 @@ import csv
 # Create frequency dictionary
 
 myDict = {}
-with open('dataset_2_with_urls.csv') as csv_file: # Choose which dataset to read from
+with open('./data/dataset_1_with_urls.csv') as csv_file: # Choose which dataset to read from
     csv_reader = csv.reader(csv_file, delimiter=',')
-    # The line counts verify that the entire file is being read
-    line_count = 0
-    count = 1
+
+    # The count verify that the entire file is being read
+
     for row in csv_reader:
-        if line_count == 0:
-            line_count += 1
-        elif(row[10].strip(" ") == "True"):  # Change what value of row you're looking for (In this case True or False)
-            print(row[10].strip(" ") + str(line_count))
-            line_count += 1
-            if(not (row[12] in myDict)): # If not in row and True
+        if(row[10].strip(" ") == "False"):  # Change what value of row you're looking for (In this case True or False)
+
+            if(not (row[12] in myDict)): # If not in row and boolean you're checking for
                 myDict[row[12]] = 1
-                print(row[10].strip(" ") + " " + str(count))
-                count += 1
+
             else:
-                print(row[10].strip(" ") + " " + str(count))
-                count += 1
                 myDict[row[12]] += 1
-    
+
+sortedDict = sorted(myDict.items(), key = lambda x: x[1], reverse=True)
 
 # Write Frequency dictionary to separate .csv
 # Choose the Output file based on inputs
-with open('TRUE_Dataset2.csv', mode='w' , newline='') as csv_file1: 
+with open('FALSE_Dataset1.csv', mode='w' , newline='') as csv_file1: 
     fieldnames = ['URLs', 'Frequency']
     writer = csv.DictWriter(csv_file1, fieldnames=fieldnames)
 
     writer.writeheader()
-    for x,y in myDict.items():
-        writer.writerow({fieldnames[0]: x, fieldnames[1]: y})
+    for x,y in sortedDict:
+        if x == "":
+            continue
+        else:
+            # Cleans the URLs of the https:// and the www.
+            if(x[8:11] == "www"):
+                writer.writerow({fieldnames[0]: x[12:26], fieldnames[1]: y})
+            else:
+                writer.writerow({fieldnames[0]: x[8:23], fieldnames[1]: y})
+
