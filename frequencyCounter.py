ad205c75028986e860b9e516a0c882eec21fc50f
Chris Wyble <cwgiant@gmail.com>
2021-02-11 00:02:41 -0600

    Touching Up Python script

--- frequencyCounter.py
+++ frequencyCounter.py
@@ -3,13 +3,13 @@ import csv
 # Create frequency dictionary
 
 myDict = {}
-with open('./data/dataset_1_with_urls.csv') as csv_file: # Choose which dataset to read from
+with open('./data/dataset_2_with_urls.csv') as csv_file: # Choose which dataset to read from
     csv_reader = csv.reader(csv_file, delimiter=',')
 
     # The count verify that the entire file is being read
 
     for row in csv_reader:
-        if(row[10].strip(" ") == "False"):  # Change what value of row you're looking for (In this case True or False)
+        if(row[10].strip(" ") == "True"):  # Change what value of row you're looking for (In this case True or False)
 
             if(not (row[12] in myDict)): # If not in row and boolean you're checking for
                 myDict[row[12]] = 1
@@ -21,7 +21,7 @@ sortedDict = sorted(myDict.items(), key = lambda x: x[1], reverse=True)
 
 # Write Frequency dictionary to separate .csv
 # Choose the Output file based on inputs
-with open('FALSE_Dataset1.csv', mode='w' , newline='') as csv_file1: 
+with open('./data/TRUE_Dataset2.csv', mode='w' , newline='') as csv_file1: 
     fieldnames = ['URLs', 'Frequency']
     writer = csv.DictWriter(csv_file1, fieldnames=fieldnames)
 
